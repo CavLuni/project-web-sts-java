2e690f44ef7d56e26b35e940fadc0b66f2f030cd
CavLuni <denis.luniere@gmail.com>
2020-04-20 17:27:32 -0300

    Tratamento de Exceção delete e update

--- src/main/java/com/cavluni/course/service/UserService.java
+++ src/main/java/com/cavluni/course/service/UserService.java
@@ -3,11 +3,16 @@ package com.cavluni.course.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cavluni.course.entities.User;
 import com.cavluni.course.repositories.UserRepository;
+import com.cavluni.course.services.exceptions.DatabaseException;
 import com.cavluni.course.services.exceptions.ResourceNotFoundExceptions;
 
 
@@ -27,16 +32,29 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repository.save(obj);
+		
+		
 	}
 
 	public void delete (Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundExceptions(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updatedata(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundExceptions(id);
+		}
 	}
 	private void updatedata(User entity, User obj) {
 		entity.setName(obj.getName());
