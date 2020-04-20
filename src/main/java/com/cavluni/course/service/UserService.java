6f9e1c0bf7bc682a02a5eef1f302525c3693c45f
CavLuni <denis.luniere@gmail.com>
2020-04-20 15:13:46 -0300

    Tratamento de Exceção findById

--- src/main/java/com/cavluni/course/service/UserService.java
+++ src/main/java/com/cavluni/course/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cavluni.course.entities.User;
 import com.cavluni.course.repositories.UserRepository;
+import com.cavluni.course.services.exceptions.ResourceNotFoundExceptions;
 
 
 @Service
@@ -21,7 +22,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundExceptions(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/cavluni/course/services/exceptions/ResourceNotFoundExceptions.java
@@ -0,0 +1,10 @@
+package com.cavluni.course.services.exceptions;
+
+public class ResourceNotFoundExceptions extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExceptions(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
