ccc8afe354347371c021af52c324a851981a266d
CavLuni <denis.luniere@gmail.com>
2020-04-20 12:39:05 -0300

    OrderItem imcrements

--- src/main/java/com/cavluni/course/entities/Order.java
+++ src/main/java/com/cavluni/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.cavluni.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.cavluni.course.entities.enums.OrderStatus;
@@ -43,6 +46,9 @@ public class Order implements Serializable {
 		setOrderStatus(orderStatus);
 		this.client = client;
 	}
+	
+	@OneToMany(mappedBy = "id.order")	
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Long getId() {
 		return id;
@@ -76,6 +82,9 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
